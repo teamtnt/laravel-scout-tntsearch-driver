621667625a2afd25c6edaedb9767911364320df3
Nenad Ticaric <nticaric@gmail.com>
2016-08-16 12:30:20 +0200

    add tests

---
+++ tests/TNTSearchEngineTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use TeamTNT\Scout\Engines\TNTSearchEngine;
+use Illuminate\Database\Eloquent\Collection;
+
+class AlgoliaEngineTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+}
+
+class TNTSearchEngineTestModel
+{
+    public function searchableAs()
+    {
+        return 'table';
+    }
+
+    public function getKey()
+    {
+        return 1;
+    }
+
+    public function toSearchableArray()
+    {
+        return ['id' => 1];
+    }
+}
