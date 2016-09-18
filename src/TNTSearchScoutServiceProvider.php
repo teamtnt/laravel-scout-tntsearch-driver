d56ca681c51a57bf7b0de62e202eaf929607d80b
Tomaz <tomaz.podgorsek@gmail.com>
2016-09-18 11:17:41 +0200

    - Verify if fuzziness config is set

--- src/TNTSearchScoutServiceProvider.php
+++ src/TNTSearchScoutServiceProvider.php
@@ -38,11 +38,13 @@ class TNTSearchScoutServiceProvider extends ServiceProvider
 
     private function setFuzziness($tnt)
     {
+        $fuzziness = config('scout.tntsearch.fuzziness');
         $prefix_length = config('scout.tntsearch.fuzzy.prefix_length');
         $max_expansions = config('scout.tntsearch.fuzzy.max_expansions');
         $distance = config('scout.tntsearch.fuzzy.distance');
 
-        $tnt->fuzziness = config('scout.tntsearch.fuzziness');
+
+        $tnt->fuzziness = isset($fuzziness) ? $fuzziness : $tnt->fuzziness;
         $tnt->fuzzy_prefix_length = isset($prefix_length) ? $prefix_length : $tnt->fuzzy_prefix_length;
         $tnt->fuzzy_max_expansions = isset($max_expansions) ? $max_expansions : $tnt->fuzzy_max_expansions;
         $tnt->fuzzy_distance = isset($distance) ? $distance : $tnt->fuzzy_distance;
