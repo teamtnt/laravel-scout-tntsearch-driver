teamtnt/laravel-scout-tntsearch-driver

commit 5a600243f8ef30b96c585ad01f3573fcc3f7910b

some claenup

src/TNTSearchScoutServiceProvider.php
@@ -1,11 +1,10 @@
-<?php
+<?php namespace TeamTNT\Scout;
 
-namespace TeamTNT\Scout;
-
-use Illuminate\Support\ServiceProvider;
-use Laravel\Scout\EngineManager;
-use TeamTNT\Scout\Console\ImportCommand;
+use Engines\TNTSearchEngine;
 use TeamTNT\TNTSearch\TNTSearch;
+use Laravel\Scout\EngineManager;
+use Illuminate\Support\ServiceProvider;
+use TeamTNT\Scout\Console\ImportCommand;
 
 class TNTSearchScoutServiceProvider extends ServiceProvider
 {
@@ -16,18 +15,19 @@ class TNTSearchScoutServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app[EngineManager::class]->extend('tntsearch', function () {
+        $this->app[EngineManager::class]->extend('tntsearch', function ($app) {
             $tnt = new TNTSearch();
+            
             $driver = config('database.default');
-            $config = config('scout.tntsearch') + config("database.connections.$driver");
-
+            $config = config('scout.tntsearch') + config("database.connections.{$driver}");
 
             $tnt->loadConfig($config);
             $tnt->setDatabaseHandle(app('db')->connection()->getPdo());
+            
             $this->setFuzziness($tnt);
             $this->setAsYouType($tnt);
 
-            return new Engines\TNTSearchEngine($tnt);
+            return new TNTSearchEngine($tnt);
         });
 
         if ($this->app->runningInConsole()) {
@@ -39,22 +39,14 @@ class TNTSearchScoutServiceProvider extends ServiceProvider
 
     protected function setFuzziness($tnt)
     {
-        $fuzziness = config('scout.tntsearch.fuzziness');
-        $prefix_length = config('scout.tntsearch.fuzzy.prefix_length');
-        $max_expansions = config('scout.tntsearch.fuzzy.max_expansions');
-        $distance = config('scout.tntsearch.fuzzy.distance');
-
-
-        $tnt->fuzziness = isset($fuzziness) ? $fuzziness : $tnt->fuzziness;
-        $tnt->fuzzy_prefix_length = isset($prefix_length) ? $prefix_length : $tnt->fuzzy_prefix_length;
-        $tnt->fuzzy_max_expansions = isset($max_expansions) ? $max_expansions : $tnt->fuzzy_max_expansions;
-        $tnt->fuzzy_distance = isset($distance) ? $distance : $tnt->fuzzy_distance;
+        $tnt->fuzziness            = config('scout.tntsearch.fuzziness', $tnt->fuzziness);
+        $tnt->fuzzy_distance       = config('scout.tntsearch.fuzzy.distance', $tnt->fuzzy_distance);
+        $tnt->fuzzy_prefix_length  = config('scout.tntsearch.fuzzy.prefix_length', $tnt->fuzzy_prefix_length);
+        $tnt->fuzzy_max_expansions = config('scout.tntsearch.fuzzy.max_expansions', $tnt->fuzzy_max_expansions);
     }
 
     protected function setAsYouType($tnt)
     {
-        $asYouType = config('scout.tntsearch.asYouType');
-
-        $tnt->asYouType = isset($asYouType) ? $asYouType : $tnt->asYouType;
+        $tnt->asYouType = config('scout.tntsearch.asYouType', $tnt->asYouType);
     }
 }

commit a776ec5a5d0b03cd98d24e05521e6b1168b07060

Fixed a bug introduced by 5a600243f8ef30b96c585ad01f3573fcc3f7910b - FatalThrowableError in TNTSearchScoutServiceProvider.php line 30: Class 'Engines\TNTSearchEngine' not found

src/TNTSearchScoutServiceProvider.php
@@ -1,10 +1,10 @@
 <?php namespace TeamTNT\Scout;
 
-use Engines\TNTSearchEngine;
 use TeamTNT\TNTSearch\TNTSearch;
 use Laravel\Scout\EngineManager;
 use Illuminate\Support\ServiceProvider;
 use TeamTNT\Scout\Console\ImportCommand;
+use TeamTNT\Scout\Engines\TNTSearchEngine;
 
 class TNTSearchScoutServiceProvider extends ServiceProvider
 {