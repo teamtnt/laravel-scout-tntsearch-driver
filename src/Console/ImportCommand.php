33f904044ff63a478ba516cf013703a212ebd4ef
Sebastian Szczepanski <sebastian@ably.pl>
2019-02-28 22:08:36 +0100

    add prefix to table name

--- src/Console/ImportCommand.php
+++ src/Console/ImportCommand.php
@@ -34,9 +34,11 @@ class ImportCommand extends Command
         $class = $this->argument('model');
 
         $model = new $class();
-        $tnt = new TNTSearch();        
+        $tnt = new TNTSearch();
         $driver = $model->getConnectionName() ?: config('database.default');
         $config = config('scout.tntsearch') + config("database.connections.$driver");
+        $tablePrefix = $config['prefix'] ?? '';
+        $tableName = $tablePrefix.$model->getTable();
 
         $tnt->loadConfig($config);
         $tnt->setDatabaseHandle(app('db')->connection($driver)->getPdo());
