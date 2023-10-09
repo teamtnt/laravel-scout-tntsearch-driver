28f1a5fd8779f17f9ef9af7616bfd73d5211b1ce
Ronald Fiering <rfiering@googlemail.com>
2023-10-09 18:58:19 +0200

    fix for searchBoolean

--- src/Engines/TNTSearchEngine.php
+++ src/Engines/TNTSearchEngine.php
@@ -218,7 +218,11 @@ class TNTSearchEngine extends Engine
         // sort models by tnt search result set
         return $model->newCollection(collect($results['ids'])->map(function ($hit) use ($models, $results) {
             if (isset($models[$hit])) {
-                return $models[$hit]->setAttribute('__tntSearchScore__', $results['docScores'][$hit]);
+                if (isset($this->tnt->config['searchBoolean']) ? $this->tnt->config['searchBoolean'] : false) {
+                    return $models[$hit];
+                } else {
+                    return $models[$hit]->setAttribute('__tntSearchScore__', $results['docScores'][$hit]);
+                }
             }
         })->filter()->all());
     }
